235b45fb80eeb2390a5b6a421a8910a64934d9cd
Lovers-1 <86130429+Lovers-1@users.noreply.github.com>
2021-08-02 15:34:30 +0200

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const About = () =>{
+    return(
+ <div className="box-area">
+<div className="banner-area">
+                <h2>About </h2>
+ </div>
+   <div className="content-area">
+                <div className="wrapper">
+                <p>Name : Lovers</p>
+     <p>Surname  :Maluleke </p>
+     <p>ID Number  :9908075652081</p>
+     <p>Gender :Male</p>
+     <p>Date Of Brith  :1999-Aug-17</p>
+     <p>NickName   :Da-luk</p>
+   
+ </div>
+            
+   </div>
+  </div>      
+ )
+}
+
+export default About;
---
+++ Landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Landing = () =>{
+    return(    
+        <>
+        <div className="box-area">
+            <div className="banner-area">
+                <h2>Lover's Resume</h2>
+            </div>
+            <div className="content-area">
+                <div className="wrapper">
+                    <h2>Wellcome Page</h2>
+                    <p>
+Welcome home! Your sweet presence makes my life happier. Welcome home and welcome back to the family! Your presence makes this house a home for me. So happy to welcome you back to the house! Having you back is such a blessing, having you away was so depressing. Welcome back! Welcome Bac</p>
+                    </div>
+            </div>
+        </div>
+        </>
+   )
+}
+
+export default Landing;
